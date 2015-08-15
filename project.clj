8b879ac5eefdd1ae4e9b30b587b6c5a598f73222
Richard Möhn <richard.moehn@posteo.de>
2015-08-15 13:03:09 +0900

    specify-cmeta-any: Include Bars from Cmeta
    Grimoire extracts all Cmetadata. Extract Bars from Cmetadata and put
    them on the Things with the other Bars.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
-(defproject org.clj-grenada/lib-grenada "0.3.2"
+(defproject org.clj-grenada/lib-grenada "0.3.3-SNAPSHOT"
   :description "A library for processing Clojure metadata"
   :url "https://github.com/clj-grenada/lib-grenada"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [dire "0.5.3"]
                  [leiningen "2.5.1"] ; Sorry for the strange dependency.
                                      ; TODO: Think about librarizing the
                                      ;       required code. (RM 2015-06-27)
@@ -11,7 +12,7 @@
                     ; Bump once Leiningen runs on Clojure 1.7.0.
                  [org.projectodd.shimdandy/shimdandy-api "1.1.0"]
                  [org.projectodd.shimdandy/shimdandy-impl "1.1.0"]
-                 [org.clj-grenada/darkestperu "0.1.1"]
+                 [org.clj-grenada/darkestperu "0.2.0-SNAPSHOT"]
                  [com.cemerick/pomegranate "0.3.0"]
 
                  ;; For new versions of guten-tag, look if the print-method
@@ -24,7 +25,8 @@
                  ;;          change’ according to Fipp's README.
                  [fipp "0.6.2"]
                  [prismatic/schema "0.4.3"]
-                 [prismatic/plumbing "0.4.4"]]
+                 [prismatic/plumbing "0.4.4"]
+                 [slingshot "0.12.2"]]
 
   :source-paths ["src/clj"]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
