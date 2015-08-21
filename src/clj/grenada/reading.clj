554a0f68e1e47aff71e3cfe18b834019aa07a93c
Richard Möhn <richard.moehn@posteo.de>
2015-08-21 11:36:00 +0900

    reading: Add doc string

--- src/clj/grenada/reading.clj
+++ src/clj/grenada/reading.clj
@@ -5,7 +5,10 @@
   (:require [clojure.edn :as edn]
             [grenada.things :as t]))
 
-(defn read-string [s]
+(defn read-string
+  "Same as clj::clojure.edn/read-string, except that it can also read Things and
+  #objects (replacing them with :unknown-object-ignored."
+  [s]
   (edn/read-string {:readers {'g/t t/vec->thing
                               'guten/tag t/vec->thing}
                     :default (fn [t v]
